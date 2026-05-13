dd2499e1c102048821883e6c9db8581a3c407f3f
karenreverberi <karenbarrazareverberi@gmail.com>
2026-05-13 20:38:44 -0300

    Refactor product input logic and variable names
    Fixed variable naming and logic errors in product input handling.

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,18 +1,19 @@
+//ejercicio 2
 let productos = []
 let precio = []
 let total = 0
 
-let producto = prompt ("ingresar nombre del producto")
+let productos = prompt ("ingresar nombre del producto")
 
 while (producto != "fin") {
-    let precio = numero(prompt ("ingresar precio del producto"))
+    let precio = numero(prompt ("ingresar precio del producto"));
 
-    productos.push (producto);
+    productos.push (productos);
     precio.push (precio);
     
-    producto = prompt ("ingresar a otro producto")      
+    productos = prompt ("ingresar a otro producto");      
 }
-console.log ("productos cargados: " + productos.length);
+console.log ("productos cargados: ");
 for (let i = 0; i < productos.length; i++) {
     console.log (productos[i] + " $" + precio[i]);
     total = total +precio[i];
