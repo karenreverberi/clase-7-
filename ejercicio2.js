a2df12231076127e71492eebfb007670d52386d4
karenreverberi <karenbarrazareverberi@gmail.com>
2026-05-13 21:00:17 -0300

    Fix variable name and improve product input handling

--- ejercicio2.js
+++ ejercicio2.js
@@ -3,20 +3,20 @@ let productos = []
 let precio = []
 let total = 0
 
-let productos = prompt ("ingresar nombre del producto")
+let producto = prompt ("ingresar nombre del producto")
 
 while (producto != "fin") {
-    let precio = numero(prompt ("ingresar precio del producto"));
+    let precio = Number(prompt ("ingresar precio del producto"));
 
-    productos.push (productos);
-    precio.push (precio);
+    productos.push(producto);
+    precio.push(precio);
     
-    productos = prompt ("ingresar a otro producto");      
+    producto = prompt ("ingresar a otro producto");      
 }
 console.log ("productos cargados: ");
 for (let i = 0; i < productos.length; i++) {
-    console.log (productos[i] + " $" + precio[i]);
-    total = total +precio[i];
+    console.log (productos[i] + "-$" + precio[i]);
+    total = total + precio[i];
 }
 console.log ("total a pagar: $" + total);
 
